1c0269c41a3eed030297df176e0aa6cf9ea9cbf0
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2023-02-16 20:25:27 -0300

    Consumindo os dados da API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+interface Array{
+  url: string;
+  description: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  photos = [];
+  photos: Array[] = [];
+
   constructor(http: HttpClient) {
-    console.log(http);
+    http
+      .get<Array[]>('http://localhost:3000/flavio/photos')
+      .subscribe((photos) => (this.photos = photos));
+      console.log(this.photos);
   }
 }
