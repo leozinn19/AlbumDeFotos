d4e68bc0075ad7812cd229e538aa7bde59a9be4d
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2023-02-17 15:14:58 -0300

    Criando o app.routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing.module';
 import { PhotosModule } from './photos/photos.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, PhotosModule],
+  imports: [BrowserModule, AppRoutingModule, PhotosModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
