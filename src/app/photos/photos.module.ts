cda8677247fb132b001747cbb056cfa238a0178a
Leonardo Oliveira <leonardo.s.o.gc@gmail.com>
2023-02-17 15:15:24 -0300

    Importando o CommonModule

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { PhotoComponent } from './photo/photo.component';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent],
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, CommonModule],
 })
 export class PhotosModule {}
